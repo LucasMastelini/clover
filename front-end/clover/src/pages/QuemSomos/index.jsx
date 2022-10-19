6e7c9edf9def14478b54671270b8bb01f94a0d3b
fabiogmsouza26 <fabio.souza@bandtec.com.br>
2022-10-19 11:48:15 -0300

    criação de pastas para componentização do institucional e inserção do código pertinente

--- front-end/clover/src/pages/QuemSomos/index.jsx
+++ front-end/clover/src/pages/QuemSomos/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import Institucional from "./components/Institucional";
+import './style.css';
+
 
 function QuemSomos() {
   return (
     <>
         <NavLink className="nav-link" to="/"><h1>Teste</h1></NavLink>
+        <Institucional></Institucional>
     </>
   )
 }
