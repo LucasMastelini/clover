daa7065e97cd0139777149c0450c863641156f0c
LucasMastelini <lucas.hotge.com@gmail.com>
2022-10-20 17:35:56 -0300

    "Tela cadastro finalizada"

--- front-end/clover/src/pages/CadastroLogin/index.jsx
+++ front-end/clover/src/pages/CadastroLogin/index.jsx
@@ -1,41 +1,52 @@
 import React, { useState, useEffect } from "react";
-import api from "../../Api/api";
+// import api from "../../Api/api";
 // import cadastro from "../../Api/cadastro";
 // import listar from "../../Api/listarUsuarios";
-import { Formik, Field, Form } from 'formik';
-import schema from '../../schema';
+// import { Formik, Field, Form } from "formik";
+// import schema from "../../schema";
 
-import './style.css';
-import userIcon from '../../assets/image/user.png'
-import emailIcon from '../../assets/image/email.png'
-import telefoneIcon from '../../assets/image/celular.png'
-import passwordIcon from '../../assets/image/senha.png'
+import "./style.css";
+// import userIcon from "../../assets/image/user.png";
+// import emailIcon from "../../assets/image/email.png";
+// import telefoneIcon from "../../assets/image/celular.png";
+// import passwordIcon from "../../assets/image/senha.png";
 import FormCadastro from "./components/FormCadastro/FormCadastro";
 import FormLogin from "./components/FormLogin/FormLogin";
 import Header from "../Header"
 function CadastroLogin() {
-   
-    // const [switchers, setSwitchers] = useState();
-    // const [isActive, setIsActive] = useState();
-    // const [cadastroIsActive, setCadastroIsActive] = useState();
-    // const [loginIsActive, setLoginIsActive] = useState();
+  // const [switchers, setSwitchers] = useState();
+  // const [isActive, setIsActive] = useState();
+  // const [cadastroIsActive, setCadastroIsActive] = useState();
+  // const [loginIsActive, setLoginIsActive] = useState();
 
+  // function cadastroSwitcher() {
+  //     setCadastroIsActive(isActive)
+  //     setLoginIsActive('form-wrapper')
+  // }
 
-    // function cadastroSwitcher() {
-    //     setCadastroIsActive(isActive)
-    //     setLoginIsActive('form-wrapper')
-    // }
+  // function loginSwitcher() {
+  //     setLoginIsActive(isActive)
+  //     setCadastroIsActive('form-wrapper')
+  // }
 
-    // function loginSwitcher() {
-    //     setLoginIsActive(isActive)
-    //     setCadastroIsActive('form-wrapper')
-    // }
+  // useEffect(() => {
+  //     setIsActive('form-wrapper is-active')
+  //     setCadastroIsActive('form-wrapper is-active')
 
-    // useEffect(() => {
-    //     setIsActive('form-wrapper is-active')
-    //     setCadastroIsActive('form-wrapper is-active')
+  // }, [])
 
-    // }, [])
+  useEffect(() => {
+
+    const switchers = [...document.querySelectorAll('.switcher')]
+
+    switchers.forEach(item => {
+        item.addEventListener('click', function () {
+            switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+            this.parentElement.classList.add('is-active')
+        })
+    })
+
+}, [])
 
 
 
