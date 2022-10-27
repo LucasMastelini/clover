9735b00d2634fa6e3e99742a67a2b756283fcf3b
fabiogmsouza26 <fabio.souza@bandtec.com.br>
2022-10-27 13:33:06 -0300

    Alterações de componentização e inserção de js

--- front-end/clover/src/routes/Router.jsx
+++ front-end/clover/src/routes/Router.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import QuemSomos from '../pages/QuemSomos'
 import CadastroLogin from '../pages/CadastroLogin'
+import CompraProduto from '../pages/CompraProduto'
 
 export default function Router() { 
   return (
@@ -11,6 +12,7 @@ export default function Router() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/quem-somos" element={<QuemSomos/>}/>
         <Route path="/cadastro-login" element={<CadastroLogin/>}/>
+        <Route path="/compra-produto" element={<CompraProduto/>}/>
       </Routes>
     // </BrowserRouter>
   )
