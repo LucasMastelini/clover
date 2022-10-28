b147d3cedb9aa7578f24d2b53f29bbf7d9ba88e8
brenda-rs <brenda.sousa@bandtec.com.br>
2022-10-27 21:31:18 -0300

    Abrindo lista de itens da categoria.

--- front-end/clover/src/pages/Header/components/iconNav/index.jsx
+++ front-end/clover/src/pages/Header/components/iconNav/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-function IconNav () {
-    return (
-        <>
-        </>
-    )
+import "./style.css";
+
+function IconNav() {
+
+  return <div className="texto">lista</div>;
 }
+
+export default IconNav
