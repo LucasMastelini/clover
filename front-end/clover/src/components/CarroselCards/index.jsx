d63dd64e9e22d9875a444d4a66b23e606ef8a8fd
leticiammarques <leticiamoura32@gmail.com>
2022-10-27 21:30:56 -0300

    animações da home page

--- front-end/clover/src/components/CarroselCards/index.jsx
+++ front-end/clover/src/components/CarroselCards/index.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {useRef} from "react";
 
 import './style.css';
 
 export default function CarroselCards({children}) {
+
+  const carousel = useRef(null);
+
+  const handLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  }
+
+  const handRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  }
+
+
   return (
     <>
       <div className="collabs-container">
-        <div className="items-collabs">
+        <div className="items-collabs" ref={carousel}>
           {children}
         </div>
 
         <div className="arrow-slides-middle bloquear-selecao">
-          <div className="arrow-right" onclick="plusDivs(-1)">&#10094;</div>
-          <div className="arrow-left" onclick="plusDivs(1)">&#10095;</div>
+          <div className="arrow-left" onClick={handLeftClick}>&#10094;</div>
+          <div className="arrow-right" onClick={handRightClick}>&#10095;</div>
         </div>
       </div>
     </>
--- front-end/clover/src/pages/HomePage/components/Comment/index.jsx
+++ front-end/clover/src/pages/HomePage/components/Comment/index.jsx
@@ -1,102 +1,59 @@
 import React from "react";
 
 import iconComment from "../../../../assets/image/iconComentario.png";
-import nota from "../../../../assets/image/nota.png"
+import nota from "../../../../assets/image/nota.png";
 
-import './style.css'
+import "./style.css";
 
-export default function Comment() {
+export default function Comment(props) {
+  const arrayDepoimentos = [
+    {
+      data: "17/10/2022",
+      texto: "Comprei por decisão do meu filho em presentear um amigo. O produto foi entregue conforme o anúncio. Saliento quanto a forma de pagamento é facilitada e a entrega rápida!!",
+      nome: "Nívea R",
+    },
+    {
+      data: "26/10/2022",
+      texto: "Gostei do site, do Instagram e do serviço de entrega e rastreamento da entrega.",
+      nome: "Beatriz M",
+    },
+    {
+      data: "10/10/2022",
+      texto: "Gostei bastante da entrega, foi rápida e correta. Só não gostei da embalagem, um saco plástico, fino. que pode ser facilmente rasgado.",
+      nome: "Elana E",
+    },
+    {
+      data: "10/10/2022",
+      texto: "Adoro os produtos! Sempre com ideias de estampas criativas dos meus heróis e personagens favoritos!!!",
+      nome: "Fernando S",
+    },
+    {
+      data: "26/10/2022",
+      texto: "Adorei que chegou tudo muito rápido, o site eh fácil de navegar, encontrei o que precisava de forma prática",
+      nome: "Mirian G",
+    },
+    {
+      data: "25/10/2022",
+      texto: "Camisetas boa e bom atendimento,vários produtos para presentear. Só deixa a desejar que os tamanhos acabam rápido",
+      nome: "Jonathan O",
+    },
+  ];
+
   return (
     <>
-      <button className="item-comments">
-        <span className="top-comments">
-          <img src={iconComment} alt="" />
-          <p>10/08/2022</p>
-        </span>
-        <span className="middle-comments">
-          <p className="text-comments">
-            Gosto bastante da loja, compro na piticas a muito tempo, tenho
-            várias camisetas e sempre lançam novidades que me atraem.
-          </p>
-          <h4>Adelmo A.</h4>
-        </span>
-        <img src={nota} alt="" />
-      </button>
-
-      <button className="item-comments">
-        <span className="top-comments">
-          <img src={iconComment} alt="" />
-          <p>10/08/2022</p>
-        </span>
-        <span className="middle-comments">
-          <p className="text-comments">
-            Gosto bastante da loja, compro na piticas a muito tempo, tenho
-            várias camisetas e sempre lançam novidades que me atraem.
-          </p>
-          <h4>Adelmo A.</h4>
-        </span>
-        <img src={nota} alt="" />
-      </button>
-
-      <button className="item-comments">
-        <span className="top-comments">
-          <img src={iconComment} alt="" />
-          <p>10/08/2022</p>
-        </span>
-        <span className="middle-comments">
-          <p className="text-comments">
-            Gosto bastante da loja, compro na piticas a muito tempo, tenho
-            várias camisetas e sempre lançam novidades que me atraem.
-          </p>
-          <h4>Adelmo A.</h4>
-        </span>
-        <img src={nota} alt="" />
-      </button>
-
-      <button className="item-comments">
-        <span className="top-comments">
-          <img src={iconComment} alt="" />
-          <p>10/08/2022</p>
-        </span>
-        <span className="middle-comments">
-          <p className="text-comments">
-            Gosto bastante da loja, compro na piticas a muito tempo, tenho
-            várias camisetas e sempre lançam novidades que me atraem.
-          </p>
-          <h4>Adelmo A.</h4>
-        </span>
-        <img src={nota} alt="" />
-      </button>
-
-      <button className="item-comments">
-        <span className="top-comments">
-          <img src={iconComment} alt="" />
-          <p>10/08/2022</p>
-        </span>
-        <span className="middle-comments">
-          <p className="text-comments">
-            Gosto bastante da loja, compro na piticas a muito tempo, tenho
-            várias camisetas e sempre lançam novidades que me atraem.
-          </p>
-          <h4>Adelmo A.</h4>
-        </span>
-        <img src={nota} alt="" />
-      </button>
-
-      <button className="item-comments">
-        <span className="top-comments">
-          <img src={iconComment} alt="" />
-          <p>10/08/2022</p>
-        </span>
-        <span className="middle-comments">
-          <p className="text-comments">
-            Gosto bastante da loja, compro na piticas a muito tempo, tenho
-            várias camisetas e sempre lançam novidades que me atraem.
-          </p>
-          <h4>Adelmo A.</h4>
-        </span>
-        <img src={nota} alt="" />
-      </button>
+      {arrayDepoimentos.map((depoimento) => (
+        <button className="item-comments">
+          <span className="top-comments">
+            <img src={iconComment} alt="" />
+            <p>{depoimento.data}</p>
+          </span>
+          <span className="middle-comments">
+            <p className="text-comments">{depoimento.texto}</p>
+            <h4>{depoimento.nome}</h4>
+          </span>
+          <img src={nota} alt="" />
+        </button>
+      ))}
     </>
   );
 }
