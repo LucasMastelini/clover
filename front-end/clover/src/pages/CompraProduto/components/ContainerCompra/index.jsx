24e325263cfa3f7b64bd1be2ca3ef3d8c55c1b35
fabiogmsouza26 <fabio.souza@bandtec.com.br>
2022-10-28 23:28:19 -0300

    Alterações em css e remodificação do slide para comportar apropriadamente react utiizando Swiper

--- front-end/clover/src/pages/CompraProduto/components/ContainerCompra/index.jsx
+++ front-end/clover/src/pages/CompraProduto/components/ContainerCompra/index.jsx
@@ -1,183 +1,19 @@
-import React, { useEffect } from "react";
-import './style.css';
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/thumbs'
+import './style.css'
+import ContainerSlider from '../ContainerSlider'
+import { productImages } from '../ContainerSlider/conteudo'
+import { BsTruck } from 'react-icons/bs';
 
 
 export default function ContainerCompra() {
 
-    useEffect(() => {
-        var corAnterior;
-
-        let images = [
-            "https://imageup.me/images/83402098-f65f-43b7-b2c2-1cd6a93a5cb8.jpeg",
-            "https://imageup.me/images/83402098-f65f-43b7-b2c2-1cd6a93a5cb8.jpeg",
-            "https://imageup.me/images/83402098-f65f-43b7-b2c2-1cd6a93a5cb8.jpeg",
-            "https://imageup.me/images/83402098-f65f-43b7-b2c2-1cd6a93a5cb8.jpeg",
-        ]
-
-        function mudarCores(event) {
-
-            if (corAnterior) {
-                corAnterior.className = "";
-            }
-
-            corAnterior = document.getElementById(event.target.id);
-            corAnterior.className = "selected";
-
-        }
-
-        //////////////////////////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////////////////////
-
-
-        function imageSlider(parent, images) {
-
-            let currentImage = 0;
-            let slider = {
-                parent: parent,
-                images: parent.querySelector(".images"),
-                thumbnails: parent.querySelector(".thumbnails"),
-                backBtn: parent.querySelector(".back-btn"),
-                nextBtn: parent.querySelector(".next-btn")
-            };
-
-            slider.images.innerHTML = images.map(function (image) {
-                return `<img src="${image}"/>`
-            }).join("");
-
-            let imageNodes = slider.images.querySelectorAll("img");
-            imageNodes[currentImage].classList.add("active");
-
-            slider.thumbnails.innerHTML = slider.images.innerHTML;
-
-            let thumbnailNodes = slider.thumbnails.querySelectorAll("img");
-            thumbnailNodes[currentImage].classList.add("active");
-
-
-            for (let i = 0; i < thumbnailNodes.length; i++) {
-                thumbnailNodes[i].addEventListener("click", function () {
-                    slider.thumbnails.querySelector("img.active").classList.remove("active");
-                    thumbnailNodes[i].classList.add("active");
-                    imageNodes[currentImage].classList.remove("active");
-                    currentImage = i;
-                    imageNodes[currentImage].classList.add("active");
-                });
-            }
-
-            slider.backBtn.addEventListener("click", function () {
-                imageNodes[currentImage].classList.remove("active");
-                currentImage--;
-                if (currentImage < 0) {
-                    currentImage = images.length - 1;
-                }
-                imageNodes[currentImage].classList.add("active");
-                slider.thumbnails.querySelector("img.active").classList.remove("active");
-                thumbnailNodes[currentImage].classList.add("active");
-            });
-
-            slider.nextBtn.addEventListener("click", function () {
-                imageNodes[currentImage].classList.remove("active");
-                currentImage = (currentImage + 1) % images.length;
-                imageNodes[currentImage].classList.add("active");
-                slider.thumbnails.querySelector("img.active").classList.remove("active");
-                thumbnailNodes[currentImage].classList.add("active");
-            });
-
-        }
-
-        imageSlider(document.querySelector(".image-slider"), images);
-
-        let cor1 = document.getElementById("cor1");
-        let cor2 = document.getElementById("cor2");
-        let cor3 = document.getElementById("cor3");
-
-
-        cor1.addEventListener('click', function handleClick() {
-
-            let contador = 0;
-
-            var imagesSlider = document.querySelectorAll(".images img");
-            var imagesThumbnail = document.querySelectorAll(".thumbnails img");
-
-            images = [
-                "../../assets/image/roupaslidebranca.png",
-                "../../assets/image/roupaslidebranca.png",
-                "../../assets/image/roupaslidebranca.png",
-                "../../assets/image/roupaslidebranca.png",
-                "../../assets/image/roupaslidebranca.png"
-            ];
-
-            for (let i = 0; i < imagesSlider.length; i++) {
-                imagesSlider[i].src = images[contador];
-                imagesThumbnail[i].src = images[contador];
-                contador++;
-            }
-        });
-
-
-        cor2.addEventListener('click', function handleClick() {
-
-            let contador = 0;
-
-            var imagesSlider = document.querySelectorAll(".images img");
-            var imagesThumbnail = document.querySelectorAll(".thumbnails img");
-
-            images = [
-                "../../assets/image/roupaslidepreta.png",
-                "../../assets/image/roupaslidepreta.png",
-                "../../assets/image/roupaslidepreta.png",
-                "../../assets/image/roupaslidepreta.png",
-                "../../assets/image/roupaslidepreta.png",
-            ];
-
-            for (let i = 0; i < imagesSlider.length; i++) {
-                imagesSlider[i].src = images[contador];
-                imagesThumbnail[i].src = images[contador];
-                contador++;
-            }
-        });
-
-
-
-        cor3.addEventListener('click', function handleClick() {
-
-            let contador = 0;
-
-            var imagesSlider = document.querySelectorAll(".images img");
-            var imagesThumbnail = document.querySelectorAll(".thumbnails img");
-
-            images = [
-                "../../assets/image/roupaslideazul.png",
-                "../../assets/image/roupaslideazul.png",
-                "../../assets/image/roupaslideazul.png",
-                "../../assets/image/roupaslideazul.png",
-                "../../assets/image/roupaslideazul.png",
-            ];
-
-            for (let i = 0; i < imagesSlider.length; i++) {
-                imagesSlider[i].src = images[contador];
-                imagesThumbnail[i].src = images[contador];
-                contador++;
-            }
-        });
-
-
-
-    }, []);
-
     return (
         <>
             <section id="produto">
-                <div className="image-slider">
-                    <div id="escopo">
-                        <div className="images"></div>
-                        <div className="back-btn">
-                            <i className="fa-solid fa-caret-left"></i>
-                        </div>
-                        <div className="next-btn">
-                            <i className="fa-solid fa-caret-right"></i>
-                        </div>
-                    </div>
-                    <div className="thumbnails"></div>
+                <div className="slides">
+                    <ContainerSlider images={productImages} />
                 </div>
                 <div id="informacoes">
                     <h1 id="titulo">Camiseta Gonew Dry Touch Fast Masculina - Branco</h1>
@@ -199,8 +35,8 @@ export default function ContainerCompra() {
                         </div>
                         <h3 id="vendidos">Vendidos: +999999</h3>
                     </div>
-                    <div id="calculoFrete">
-                        <i className="fa-solid fa-truck"></i>
+                    <div className="calculoFrete">
+                    <BsTruck />
                         <h4 className="tituloCalcularFrete">calcular o frete</h4>
                         <div id="calcular">
                             <input className="inputCalcular" placeholder="Insira o CEP" />
