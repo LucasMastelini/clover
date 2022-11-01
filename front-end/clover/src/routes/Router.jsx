d6ae9be63108b271d573a0322553c100a9cb6c76
brenda-rs <brenda.sousa@bandtec.com.br>
2022-10-31 21:39:50 -0300

    resolvendo conflitos

--- front-end/clover/src/routes/Router.jsx
+++ front-end/clover/src/routes/Router.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import QuemSomos from '../pages/QuemSomos'
 import CadastroLogin from '../pages/CadastroLogin'
-// import CompraProduto from '../pages/CompraProduto'
+import CompraProduto from '../pages/CompraProduto'
 
 export default function Router() {
   return (
@@ -11,7 +11,7 @@ export default function Router() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/quem-somos" element={<QuemSomos/>}/>
         <Route path="/cadastro-login" element={<CadastroLogin/>}/>
-        {/* <Route path="/compra-produto" element={<CompraProduto/>}/> */}
+        <Route path="/compra-produto" element={<CompraProduto/>}/>
         {/* <Route path="/resumo-pedidos/:produtos" element={<ResumoPedidos />} /> */}
       </Routes>
   )
