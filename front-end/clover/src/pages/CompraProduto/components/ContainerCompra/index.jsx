216a9e063af155573b65a91061e31bcffc9205c8
fabiogmsouza26 <fabio.souza@bandtec.com.br>
2022-11-03 20:16:39 -0300

    Imagens mudando dinamicamente no slider conforme cor escolhida

--- front-end/clover/src/pages/CompraProduto/components/ContainerCompra/index.jsx
+++ front-end/clover/src/pages/CompraProduto/components/ContainerCompra/index.jsx
@@ -9,11 +9,20 @@ import React, { useEffect, useState } from 'react'
 import Rate from '../Rate'
 
 
+import img1 from '../ContainerSlider/conteudo/images/slider01.jpg';
+import img2 from '../ContainerSlider/conteudo/images/slider02.jpg';
+import img3 from '../ContainerSlider/conteudo/images/slider03.jpg';
+import img4 from '../ContainerSlider/conteudo/images/slider04.jpg';
+import img5 from '../ContainerSlider/conteudo/images/slider05.jpg';
+import img6 from '../ContainerSlider/conteudo/images/slider06.jpg';
+import img7 from '../ContainerSlider/conteudo/images/spider-man-pc.jpg';
+
+
 export default function ContainerCompra() {
 
     const [rating, setRating] = useState(0);
     const [rating2, setRating2] = useState(0);
-    
+
     const [show, setShow] = useState(false);
     const onClick = () => setShow(true);
 
@@ -21,11 +30,69 @@ export default function ContainerCompra() {
         console.log(show);
     }, [show]);
 
+    const [vetorImagem, setVetorImagem] = useState([])
+
+    const [imagens, setImagens] = useState([
+        img1,
+        img2,
+        img3,
+        img4,
+        img5,
+        img6
+    ]);
+
+            // img = [
+            //     {
+            //         id: 1,
+            //         url: "url1",
+            //         cor: "branco"
+            //     },
+            //     {
+            //         id: 2,
+            //         url: "url2",
+            //         cor: "amarelo"
+            //     },
+            //     {
+            //         id: 3,
+            //         url: "url3",
+            //         cor: "preto"
+            //     }
+            // ]
+
+    function escolherCor(corEscolhida) {
+        if (corEscolhida === 1) {
+
+            setImagens([
+                img1,
+                img2,
+                img3,
+                img4,
+                img5,
+                img6
+            ])
+        } else if (corEscolhida === 2) {
+            setImagens([
+                img7,
+                img7,
+                img7,
+                img7,
+                img7,
+                img7
+            ])
+        }
+        else {
+            setImagens([
+                img5,
+                img6
+            ])
+        }
+    }
+
     return (
         <>
             <section className="produto">
                 <div className="slides">
-                    <ContainerSlider images={productImages} />
+                    <ContainerSlider images={imagens} />
                 </div>
                 <div className="informacoes">
                     <h1 className="titulo">Camiseta Gonew Dry Touch Fast Masculina - Branco</h1>
@@ -48,11 +115,11 @@ export default function ContainerCompra() {
                     <div className="cores">
                         <h3>Selecione a cor</h3>
                         <div className="opcoesCores">
-                            <div className="cor1">
+                            <div className="cor1" onClick={() => escolherCor(1)}>
                             </div>
-                            <div className="cor2">
+                            <div className="cor2" onClick={() => escolherCor(2)}>
                             </div>
-                            <div className="cor3">
+                            <div className="cor3" onClick={() => escolherCor(3)}>
                             </div>
                         </div>
                     </div>
@@ -86,4 +153,3 @@ export default function ContainerCompra() {
 }
 
 const Text = () => <div>You clicked the button!</div>;
-
