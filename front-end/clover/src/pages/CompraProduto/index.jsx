813c222b9b80313ba5e856bb3e45a933bb913b48
leticiammarques <leticiamoura32@gmail.com>
2022-11-03 21:25:37 -0300

    recuperação da tela de compra de produto

---
+++ front-end/clover/src/pages/CompraProduto/index.jsx
@@ -0,0 +1,18 @@
+
+import ContainerCompra from "./components/ContainerCompra";
+import ContainerDescricao from "./components/ContainerDescricao";
+
+import './style.css';
+
+function CompraProduto() {
+
+    return (
+        <>
+            <ContainerCompra />
+            <ContainerDescricao />
+            </>
+
+            );
+        }
+
+    export default CompraProduto;
