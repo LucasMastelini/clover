21728565030ac1342469621a849b6a96f08dec60
brenda-rs <brenda.sousa@bandtec.com.br>
2022-11-04 01:22:21 -0300

    itens carrinho mapeado

--- front-end/clover/src/pages/Carrinho/index.jsx
+++ front-end/clover/src/pages/Carrinho/index.jsx
@@ -1,47 +1,44 @@
 import React from "react";
+import Header from "../Header";
 
-import"./style.css";
+import "./style.css";
 
 function Carrinho(props) {
-
-    const itens = [
-        {
-          id: 1,
-          titulo: "Camiseta",
-          valor: 79.99,
-          descricao: "Uma camiseta branca lisa",
-        },
-        {
-          id: 2,
-          titulo: "Calça Jeans preta",
-          valor: 65.0,
-          descricao: "Deans colado",
-        },
-        {
-          id: 3,
-          titulo: "Air Nex",
-          valor: -99.0,
-          descricao: "Tenis mike original",
-        },
-      ];
-
+  const itens = [
+    {
+      id: 1,
+      titulo: "Camiseta",
+      valor: 79.99,
+      descricao: "Uma camiseta branca lisa",
+    },
+    {
+      id: 2,
+      titulo: "Calça Jeans preta",
+      valor: 65.0,
+      descricao: "Deans colado",
+    },
+    {
+      id: 3,
+      titulo: "Air Nex",
+      valor: -99.0,
+      descricao: "Tenis mike original",
+    },
+  ];
 
   return (
-
     <>
-    {itens.map((item) => )}
-    <div key={props[0].id}>
-      Nome produto: {props[0].nome}
-      Valor R$ {props[0].preco}
-    </div>
-    <div className="content-carrinho">
-        <h1>
-            oi meu amooooooor
-        </h1>
-    </div>
+    <Header/>
+      {itens.map((item) => (
+        <div className="contem-carrinho" key={item.id} >
+          Nome produto: {item.titulo}
+          Valor R$ {item.valor}
+        </div>
+      ))}
+      <div className="contem-carrinho">
+        <h1>oi meu amooooooor</h1>
+      </div>
     </>
-    
   );
-};
+}
 
 export default Carrinho;
