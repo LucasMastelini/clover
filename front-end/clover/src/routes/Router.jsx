ad4bfd9ebc7f25a0219961971618febaad7c5e31
LucasMastelini <lucas.hotge.com@gmail.com>
2022-11-09 21:26:34 -0300

    atuazização final

--- front-end/clover/src/routes/Router.jsx
+++ front-end/clover/src/routes/Router.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import QuemSomos from '../pages/QuemSomos'
+import Usuario from '../pages/Usuario'
 import CadastroLogin from '../pages/CadastroLogin'
 import CompraProduto from '../pages/CompraProduto'
-// import PedidoRealizado from '../pages/PedidoRealizado'
+import PedidoRealizado from '../pages/PedidoRealizado'
+import Carrinho from '../pages/Carrinho'
+
 export default function Router() {
   return (
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/quem-somos" element={<QuemSomos/>}/>
         <Route path="/cadastro-login" element={<CadastroLogin/>}/>
-        <Route path="/cadastro-login" element={<CompraProduto/>}/>
-        {/* <Route path="/cadastro-login" element={<PedidoRealizado/>}/> */}
-        {/* <Route path="/compra-produto" element={<CompraProduto/>}/> */}
+        <Route path="/compra-produto" element={<CompraProduto/>}/>
+        <Route path="/pedido-realizado" element={<PedidoRealizado/>}/>
+        <Route path="/usuario" element={<Usuario/>}/>
+        <Route path="/carrinho" element={<Carrinho/>}/>
         {/* <Route path="/resumo-pedidos/:produtos" element={<ResumoPedidos />} /> */}
       </Routes>
   )
