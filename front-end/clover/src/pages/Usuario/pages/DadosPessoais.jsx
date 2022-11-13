6b66c05599646a2d5bddef4872161f1d9e305682
fabiogmsouza26 <fabio.souza@bandtec.com.br>
2022-11-13 15:05:45 -0300

    Alterações em css pertinentes a disposição dos elementos e cores, adição de botão e de inputs

--- front-end/clover/src/pages/Usuario/pages/DadosPessoais.jsx
+++ front-end/clover/src/pages/Usuario/pages/DadosPessoais.jsx
@@ -1,4 +1,4 @@
-import {React, useState} from 'react'
+import { React, useState } from 'react'
 import Header from '../../Header'
 import SideBar from '../Sidebar/SideBar';
 import { BiUser } from 'react-icons/bi';
@@ -13,19 +13,66 @@ function DadosPessoais() {
       <Header />
       <div className="container-geral">
         <SideBar></SideBar>
-        <div className="container-dados-pessoais">
+        <form className="container-dados-pessoais">
           <div className="dados-pessoais">
             <div className="title">
               <BiUser className="icon-user" /><h1>Dados pessoais</h1>
             </div>
 
             <div className="container-informacoes">
-            <input value={countryName} 
-            onChange={event => {setCountryName(event.target.value)}} 
-            variant="outlined" InputLabelProps={{shrink: true}} />
+              <label>
+                Nome
+                <input value={countryName}
+                  onChange={event => { setCountryName(event.target.value) }}
+                  InputLabelProps={{ shrink: true }} />
+              </label>
+
+              <label>
+                Sobrenome
+                <input value={countryName}
+                  onChange={event => { setCountryName(event.target.value) }}
+                  InputLabelProps={{ shrink: true }} />
+              </label>
+
+              <label className='email'>
+                E-mail
+                <input value={countryName}
+                  onChange={event => { setCountryName(event.target.value) }}
+                  InputLabelProps={{ shrink: true }} />
+              </label>
+
+              <label>
+                CPF
+                <input value={countryName}
+                  onChange={event => { setCountryName(event.target.value) }}
+                  InputLabelProps={{ shrink: true }} />
+              </label>
+
+              <label>
+                Gênero
+                <input value={countryName}
+                  onChange={event => { setCountryName(event.target.value) }}
+                  InputLabelProps={{ shrink: true }} />
+              </label>
+
+              <label>
+                Data de nascimento
+                <input value={countryName}
+                  onChange={event => { setCountryName(event.target.value) }}
+                  InputLabelProps={{ shrink: true }} />
+              </label>
+
+              <label>
+                Celular
+                <input value={countryName}
+                  onChange={event => { setCountryName(event.target.value) }}
+                  InputLabelProps={{ shrink: true }} />
+              </label>
+
+              <button>Salvar</button>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </>
   )
