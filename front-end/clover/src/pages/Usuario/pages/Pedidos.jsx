de076cc6aa507128289a424c304d5d6375c5409e
fabiogmsouza26 <fabio.souza@bandtec.com.br>
2022-11-16 03:11:09 -0300

    Implementação de modal e ajustes de componentização, bem como ajustes de css.

--- front-end/clover/src/pages/Usuario/pages/Pedidos.jsx
+++ front-end/clover/src/pages/Usuario/pages/Pedidos.jsx
@@ -2,27 +2,17 @@ import React from 'react'
 import Header from '../../Header'
 import SideBar from '../Sidebar/SideBar';
 import './style-pedidos.css'
-import Spiderman from './images/spiderman.jpg'
+import CardPedidos from './CardPedidos/CardPedidos';
 
 function Pedidos() {
+
   return (
     <>
       <Header />
       <div className="container-geral">
         <SideBar></SideBar>
         <div className="container-pedidos">
-          <div className="pedidos">
-            <div className="card">
-              <img src={Spiderman} />
-              <div className="infos-pedidos">
-                <div className="item-pedido">
-                  <p className="pedido-titulo">Pedido:</p>
-                  <p className="pedido-numero">UC1111111P</p>
-                </div>
-                <p className="pedido-titulo">Destinatario: Julia</p>
-              </div>
-            </div>
-          </div>
+          <CardPedidos />
         </div>
       </div>
     </>
