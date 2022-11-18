5d8cda5a55f565da0abb10ed8a577d96711f16a0
fabiogmsouza26 <fabio.souza@bandtec.com.br>
2022-11-18 04:42:09 -0300

    Remoção de rotas não mais utilizadas

--- front-end/clover/src/routes/Router.jsx
+++ front-end/clover/src/routes/Router.jsx
@@ -6,13 +6,6 @@ import Usuario from '../pages/Usuario'
 import CadastroLogin from '../pages/CadastroLogin'
 import CompraProduto from '../pages/CompraProduto'
 import PedidoRealizado from '../pages/PedidoRealizado'
-import Carrinho from '../pages/Carrinho'
-
-import Pedidos from '../../src/pages/Usuario/pages/Pedidos'
-import Cartoes from '../../src/pages/Usuario/pages/Cartoes'
-import DadosPessoais from '../../src/pages/Usuario/pages/DadosPessoais'
-import Enderecos from '../../src/pages/Usuario/pages/Enderecos'
-import Sair from '../../src/pages/Usuario/pages/Sair'
 
 export default function Router() {
   return (
@@ -22,15 +15,7 @@ export default function Router() {
       <Route path="/cadastro-login" element={<CadastroLogin />} />
       <Route path="/compra-produto" element={<CompraProduto />} />
       <Route path="/pedido-realizado" element={<PedidoRealizado />} />
-
-      
-      <Route path="/pedidos" element={<Pedidos />} />
       <Route path="/usuario" element={<Usuario />} />
-      <Route path="/carrinho" element={<Carrinho />} />
-      <Route path="/cartoes" element={<Cartoes />} />
-      <Route path="/dados-pessoais" element={<DadosPessoais />} />
-      <Route path="/enderecos" element={<Enderecos />} />
-      <Route path="/sair" element={<Sair />} />
       {/* <Route path="/resumo-pedidos/:produtos" element={<ResumoPedidos />} /> */}
     </Routes>
   )
