0c11ea44fd79f281193e881f6c34863e5208d940
leticiammarques <leticiamoura32@gmail.com>
2022-11-22 13:17:50 -0300

    tela de endereço finalizada

---
+++ front-end/clover/src/pages/FinalizarCompra/components/OpcoesCadastradas/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BsHouseFill } from "react-icons/bs";
+import { AiFillEdit } from "react-icons/ai";
+import { FaTrash } from "react-icons/fa";
+
+import "./style.css";
+
+export default function OpcoesCadastradas(props) {
+  return (
+    <>
+      <div className="card-opcoes-cadastradas">
+        <BsHouseFill /*{props.icon}*//>
+        <div className="corpo-card-opcoes-cadastradas">
+          <p>{props.logradouro}, {props.number}</p>
+          <p>CEP: {props.cep}</p>
+          <p>{props.cidade} - {props.uf}</p>
+        </div>
+        <div className="icons-opcoes-cadastradas">
+          <AiFillEdit />
+          <FaTrash className="icon-lixeira" />
+        </div>
+      </div>
+    </>
+  );
+}
