5ea652d70f3623e5e1894c2fa420f25327a75492
leticiammarques <leticiamoura32@gmail.com>
2022-12-08 18:14:43 -0300

    tela de admin okay

--- front-end/clover/src/pages/TelaAdmin/index.jsx
+++ front-end/clover/src/pages/TelaAdmin/index.jsx
@@ -3,72 +3,22 @@ import { useState } from "react";
 import { AiOutlineCloudDownload, AiOutlineCloudUpload } from "react-icons/ai";
 import { FiLogOut } from "react-icons/fi";
 import { FaTshirt } from "react-icons/fa";
-import { PercentualFaturamentoPorRegiao, TaxaConversao, TiketMedio, UserData } from "./Data";
+import { UserData } from "./Data";
 import logoClover from "../../assets/image/logo.png";
 import BarCharts from "./components/BarCharts";
 import LineCharts from "./components/LineCharts";
 import PieCharts from "./components/PieCharts";
-import { useNavigate } from "react-router-dom";
+import api from "../../Api/api"
 
 import "./style.css";
-import api from "../../Api/api";
-import { useEffect } from "react";
 
 export default function TelaAdmin() {
-  const navegar = useNavigate();
-  const [admin , setAdmin] = useState(false);
-
-
-  // useEffect(() => {
-  //   var array =[];
-  //   api.get('/admin/dashboard')
-  //     .then((res) => {
-  //       console.log(res.data);
-  //       setFaturamento(res.data.percentualFaturamentoPorRegiao.lista);
-  //       for(let i = 0; i < res.data.length; i++) {
-  //         array.push(res.data[i])
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     })
-
-
-  //     console.log(array.length);
-  //     console.log(array.lista);
-  //     setAdmin(localStorage.getItem('isLogado'));
-  // }, [])
-  // console.log(faturamento?.map((data) => data.regiao));
-
-
-
-
-  // const [dados] = useState({
-  //   labels: faturamento?.map((data) => data.regiao),
-  //   datasets: [
-  //     {
-  //       label: "Percentual faturado",
-  //       data: faturamento?.map((data) => data.percentualFaturamentoPorRegiao),
-  //       backgroundColor: [
-  //         "#35DAF0",
-  //         "#510090",
-  //         "#37003D",
-  //         "#082166",
-  //         "#C468CD",
-  //       ],
-  //       borderColor: "black",
-  //       borderWidth: 1,
-  //       color: "#fff",
-  //     },
-  //   ],
-  // });
-  
-  const [faturamento, setFaturamento] = useState({
-    labels: PercentualFaturamentoPorRegiao.map((data) => console.log(data.percentualFaturamento) ),
+  const [userData] = useState({
+    labels: UserData.map((data) => data.year),
     datasets: [
       {
-        label: "Percentual faturado",
-        data: PercentualFaturamentoPorRegiao.map((data) => data.regiao),
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
         backgroundColor: [
           "#35DAF0",
           "#510090",
@@ -83,115 +33,64 @@ export default function TelaAdmin() {
     ],
   });
 
-  const [conversao, setConversao] = useState({
-    labels: TaxaConversao.map((data) => console.log(data.taxaConversao)),
-    datasets: [
-      {
-        label: "Percentual faturado",
-        data: TaxaConversao.map((data) => data.data),
-        backgroundColor: [
-          "#35DAF0",
-          "#510090",
-          "#37003D",
-          "#082166",
-          "#C468CD",
-        ],
-        borderColor: "black",
-        borderWidth: 1,
-        color: "#fff",
-      },
-    ],
-  });
+  const extensao = "csv"
 
-  const [tiket, setTiket] = useState({
-    labels: TiketMedio.map((data) => console.log(data.ticketMedio)),
-    datasets: [
-      {
-        label: "Percentual faturado",
-        data: TaxaConversao.map((data) => data.mes),
-        backgroundColor: [
-          "#35DAF0",
-          "#510090",
-          "#37003D",
-          "#082166",
-          "#C468CD",
-        ],
-        borderColor: "black",
-        borderWidth: 1,
-        color: "#fff",
-      },
-    ],
-  });
-
-  function sair() {
-
-    const values = {
-      id: localStorage.getItem('id'),
-      nome: localStorage.getItem('nome'),
-      email: localStorage.getItem('email')
-    }
-
-    api.post('/logoff', values)
-      .then((res) => {
-        console.log(res);
-        localStorage.clear();
-        navegar('/')
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+  function baixarArquivo(event) {
+    api.get(`gravar-arquivos/${extensao}`, event).then((res) => {
+            console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
   }
 
-  if (admin) {
-    return (
-      <>
-        <div className="container-admin">
-          <div className="header-admin">
-            <div className="container-arquivos">
-              <button className="upload-button">
-                <a href="http://localhost:3000/" target="_blank" rel="noopener noreferrer" >
-                  <AiOutlineCloudDownload />
-                  <h3>Download</h3>
+  return (
+    <>
+      <div className="container-admin">
+        <div className="header-admin">
+          <div className="container-arquivos">
+            <button className="upload-button">
+                <a href="" target="_blank" rel="noopener noreferrer" >
+                    <AiOutlineCloudDownload />
+                    <h3>Download</h3>
                 </a>
-              </button>
-              <button className="upload-button">
+            </button>
+            <button className="upload-button">
                 <a href="http://localhost:3001/" target="_blank" rel="noopener noreferrer">
-                  <AiOutlineCloudUpload />
-                  <h3>Upload</h3>
+                    <AiOutlineCloudUpload />
+                    <h3>Upload</h3>
                 </a>
-              </button>
-            </div>
-            <img src={logoClover} alt="" />
-            <button className="upload-button" onClick={sair}>
-              <h3>Sair</h3>
-              <FiLogOut />
             </button>
           </div>
-          <div className="body-admin">
-            <div className="topo-body">
-              <FaTshirt />
-              <h3>
-                Quantidade de produtos total: <span>20</span>
-              </h3>
+          <img src={logoClover} alt="" />
+          <button className="upload-button">
+            <h3>Sair</h3>
+            <FiLogOut />
+          </button>
+        </div>
+        <div className="body-admin">
+          <div className="topo-body">
+            <FaTshirt />
+            <h3>
+              Quantidade de produtos total: <span>20</span>
+            </h3>
+          </div>
+          <div className="body-charts">
+            <div className="chart">
+              <BarCharts chartData={userData} />
             </div>
-            <div className="body-charts">
-              <div className="chart">
-                <BarCharts chartData={faturamento} />
-              </div>
-              <div className="chart">
-                <LineCharts chartData={conversao} />
-              </div>
-              <div className="chart">
-                <PieCharts chartData={faturamento} />
-              </div>
+            <div className="chart">
+              <LineCharts chartData={userData} />
+            </div>
+            <div className="chart">
+              <PieCharts chartData={userData} />
             </div>
           </div>
-          {/* <div className="area-upload">
+        </div>
+        {/* <div className="area-upload">
           <AreaUpload />
         </div> */}
-        </div>
-      </>
-    );
-  } else{ navegar("/")}
-
+      </div>
+    </>
+  );
 }
